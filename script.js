3897f9f84af0b90152ffc1ff98bdc8d26b7dcb59
abdulalkout <abdul.alkout@gmail.com>
2023-10-30 21:02:30 -0500

    DOM Menu Lab 1

---
+++ script.js
@@ -0,0 +1,32 @@
+// Data Structure
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+// Queries
+const mainEl = document.querySelector("main");
+const header = document.querySelector("header");
+const topMenuEl = document.createElement("nav");
+
+// Functions
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+mainEl.className = "flex-ctr";
+
+topMenuEl.setAttribute("id", "top-menu");
+topMenuEl.className = "flex-around";
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+header.appendChild(topMenuEl);
+
+menuLinks.forEach((element) => {
+  let newAnc = document.createElement("a");
+  newAnc.href = element.href;
+  newAnc.textContent = element.text;
+  topMenuEl.appendChild(newAnc);
+});
+
+// Event Listners
